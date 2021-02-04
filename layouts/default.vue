77b3147bbb29ac220511965ab1b65d358cf36bea
Nemanja Balac <nbposta@gmail.com>
2021-02-04 17:02:18 +0100

    test-init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-app(dark)
+    v-app-bar(color="rs__primary" fixed app).font-weight-medium.white--text.elevation-4
+      v-toolbar-title(v-text="title")
+    v-main
+      nuxt
+    v-footer(app)
+      span.mx-auto {{title}} &copy; {{ new Date().getFullYear() }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Resights'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col(cols)
+        DataTable(:headers="headers" :items="users")
+</template>
+
+<script>
+import DataTable from '~/components/DataTable.vue'
+import users from '~/api/users.js'
+
+export default {
+  components: {
+    DataTable
+  },
+  data() {
+    return {
+      users: null,
+      headers: [
+        { text: 'User', value: 'name', align: 'start' },
+        { text: 'Age', value: 'age' },
+        { text: 'Location', value: 'location' },
+        { text: 'Phone', value: 'phone' },
+        { text: 'Username', value: 'login' },
+        { text: 'Email', value: 'email' },
+      ],
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      const data = users.results
+      this.users = data
+    }
+  }
+}
+</script>
