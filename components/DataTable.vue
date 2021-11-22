91f979c044a6054c49ff28f66f133e57bdffd9b1
colin <colinwitkamp@gmail.com>
2021-11-22 15:40:03 -0800

    components/DataTable: render search and filter

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,24 +1,63 @@
 <template lang="pug">
-  v-data-table(
-    :headers="headers"
-    :items="items"
-    item-key="email"
-    dense
-  ).elevation-1.mt-10
+v-container
+  v-toolbar.mb-1(dark="", color="blue darken-3")
+    v-text-field(
+      v-model="search",
+      clearable="",
+      flat="",
+      solo-inverted="",
+      hide-details="",
+      prepend-inner-icon="mdi-magnify",
+      label="Search"
+    )
+  v-data-table.elevation-1(
+    :headers="headers",
+    :items="items",
+  )
+
+    template(v-slot:body.prepend='{ headers }')
+      tr
+        td
+        td
+        td
+          v-select(:items='filters.gender' :model='gender' label='Filter')
+        td
+        td
+        td
+
+    template(v-slot:item.user='{ item }')
+      | {{ item.user.first_name }} {{ item.user.last_name }}
+
 </template>
 
 <script>
 export default {
-  props: ['headers', 'items'],
+  props: ["headers", "items"],
   data() {
+
     return {
-      
-    }
-  }
-}
+      keys: [
+        { text: "Name", value: "user", align: "start" },
+        { text: "Email", value: "email" },
+        { text: "Gender", value: "gender" },
+        { text: "Year", value: "year" },
+        { text: "Sales", value: "sales" },
+        { text: "Country", value: "country" },
+      ],
+      filters: {
+        gender: ["all", "male", "female"],
+      },
+      search: "",
+      gender: "all",
+      pagination: {
+        sortBy: "name",
+      },
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-  .v-data-table
-    max-width: 100%
+.v-data-table
+  max-width: 100%
 </style>
