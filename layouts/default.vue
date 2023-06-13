7aca22114834c246a136d73d40d434ed19aa145b
KarolinaJab <karolina@resights.dk>
2023-06-13 10:20:05 +0200

    Remove pug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
-<template lang="pug">
-  v-app(dark)
-    v-app-bar(color="rs__primary" fixed app).font-weight-medium.white--text.elevation-4
-      v-toolbar-title(v-text="title")
-    v-main
-      nuxt
-    v-footer(app)
-      span.mx-auto {{title}} &copy; {{ new Date().getFullYear() }}
+<template>
+<v-app dark>
+  <v-app-bar class="font-weight-medium white--text elevation-4" color="rs__primary" fixed="fixed" app="app">
+      <v-toolbar-title :v-text="title"></v-toolbar-title>
+  </v-app-bar>
+  <v-main>
+      <nuxt></nuxt>
+  </v-main>
+  <v-footer app="app"><span class="mx-auto">{{title}} &copy; {{ new Date().getFullYear() }}</span></v-footer>
+</v-app>
 </template>
 
 <script>
